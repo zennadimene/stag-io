16cce0cd498e63b8eefee4e6140a0926e98d7f5d
zennadimene <zennadimene@github.com>
2026-05-19 17:13:24 +0100

    update database config

--- config/database.js
+++ config/database.js
@@ -3,20 +3,16 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-    //host: process.env.DB_HOST || 'localhost',
-    host: process.env.DB_HOST,
-    //user: process.env.DB_USER || 'root',
-    user: process.env.DB_USER,
-    //password: process.env.DB_PASSWORD || '',
-    //database: process.env.DB_NAME || 'stag_io_platform',
-    //port: process.env.DB_PORT || 3306,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-    //add
-    ssl: {
-    rejectUnauthorized: false
-},
+    host: process.env.DB_HOST || 'localhost',
+    //host: process.env.DB_HOST,
+    user: process.env.DB_USER || 'root',
+    //user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'stag_io_platform',
+    port: process.env.DB_PORT || 3306,
+    //password: process.env.DB_PASSWORD,
+    //database: process.env.DB_NAME,
+    //port: process.env.DB_PORT,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
